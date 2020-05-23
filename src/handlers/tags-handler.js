2edea0f730c5b69947a3a60e7fa1575114d80f1d
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-05-23 08:33:41 +0100

    spell checker standalone

--- src/handlers/tags-handler.js
+++ src/handlers/tags-handler.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const Tags = require('../models/tags');
+const SpellChecker = require('simple-spellchecker');
 
+const Dictionary = SpellChecker.getDictionarySync('en-GB');
 
 /**
  * Get tags.
@@ -12,9 +14,42 @@ const Tags = require('../models/tags');
 const getTags = (request, h) => {
 
     const inputText = request.query.text;
+    const words = inputText.split(/([_\W])/);
+    const suggestions = [];
     const tags = Tags.newTags();
     tags.inputText = inputText;
-    tags.tags = ['TODO1', 'TODO2'];
+    words
+        .filter((word) => word.trim().length > 0)
+        .forEach((word) => {
+
+            if (!Dictionary.spellCheck(word)) {
+                suggestions.push(Dictionary.getSuggestions(word));
+            }
+            else {
+                suggestions.push([word]);
+            }
+        });
+    const totalSuggestions = suggestions.length;
+    const suggestionTexts = [];
+    for (let i = 0; i < 1; ++i) {
+        let text = '';
+        for (let j = 0; j <= totalSuggestions; ++j) {
+            const suggArr = suggestions[j];
+            if (!suggArr || i >= suggArr.length) {
+                text = text + '';
+            }
+            else {
+                text = text + ' ' + suggArr[i];
+            }
+        }
+
+        if (text) {
+            suggestionTexts.push(text.trim());
+            suggestionTexts.push(inputText.trim());
+        }
+    }
+
+    tags.tags = suggestionTexts;
     return h.response(tags);
 };
 
