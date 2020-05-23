de143a554214374f3d4d47d7b1278a53ef0f3683
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-05-23 03:39:20 +0100

    eslint fixes

--- app.js
+++ app.js
@@ -5,6 +5,5 @@ const Server = require('./src/server/server');
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 7777;
 
-console.log(' Env vars: ' + JSON.stringify(process.env));
 // Start the server with the host and port specified as passed-in arguments
 module.exports = Server.start(host, port);
